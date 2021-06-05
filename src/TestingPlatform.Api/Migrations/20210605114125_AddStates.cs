fd8b4b331146552e573209158ca7203bbdeed414
Григорьев Вадим Алексеевич <grigorev.v@skbkontur.ru>
2021-06-05 14:48:00 +0300

    migration

---
+++ src/TestingPlatform.Api/Migrations/20210605114125_AddStates.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestingPlatform.Api.Migrations
+{
+    public partial class AddStates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Tests",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Questions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Tests");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Questions");
+        }
+    }
+}
